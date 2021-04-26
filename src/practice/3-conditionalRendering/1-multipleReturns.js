cf28f6496e753cc79ac01092dbae551aec56283e
swostika61 <swostikalamichhane61@gmail.com>
2021-04-26 20:31:11 +0545

    multiple returns

--- src/practice/3-conditionalRendering/1-multipleReturns.js
+++ src/practice/3-conditionalRendering/1-multipleReturns.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+const url = 'https://api.github.com/users/swostika61a';
 
 const MultipleReturns = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const [user, setUser] = useState('default user');
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => {
+                if(response.status >=200 && response.status <=200){
+                   return response.json()
+                }
+                else{
+                    setIsLoading(false);
+                    setIsError(true);
+                    throw new Error(response.statusText);
+                }
+            })
+            .then((user) => {
+                const {login} = user;
+                setUser(login);
+                setIsLoading(false);
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+    if (isLoading) {
+        return (
+            <div>
+                <h1>loading...</h1>
+            </div>
+        )
+    }
+    if (isError) {
+        return (
+            <div>
+                <h1>Error...</h1>
+            </div>
+        )
+    }
     return (
         <>
-            <h1>conditional rendering</h1>
+            <h2>{user}</h2>
         </>
     )
 }
